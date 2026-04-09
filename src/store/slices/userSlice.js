53e9544474ee7a74fc1cc89e38fe355de66f2756
S MADHUSUDHANAN <smadhusudhanan63@gmail.com>
2026-04-09 15:38:37 +0530

    Update userSlice.js

--- src/store/slices/userSlice.js
+++ src/store/slices/userSlice.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 import { toggleAddNewAdminPopup } from './popUpSlice';
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 const userSlice = createSlice({
     name: "user",
     initialState: {
@@ -69,7 +71,7 @@ export const {
 
 export const fetchAllUsers = () => async (dispatch) => {
     dispatch(userSlice.actions.fetchAllUsersRequest());
-    await axios.get("http://localhost:4000/api/v1/user/all", { withCredentials: true })
+    await axios.get(`${API_URL}/user/all`, { withCredentials: true })
         .then((res) => {
             dispatch(userSlice.actions.fetchAllUsersSuccess(res.data.users));
         })
@@ -80,7 +82,7 @@ export const fetchAllUsers = () => async (dispatch) => {
 
 export const addNewAdmin = (data) => async (dispatch) => {
     dispatch(userSlice.actions.addNewAdminRequest());
-    await axios.post("http://localhost:4000/api/v1/user/add/new-admin", data,
+    await axios.post(`${API_URL}/user/add/new-admin`, data,
         {
             withCredentials: true,
             headers: { "Content-Type": "multipart/form-data" },
@@ -99,7 +101,7 @@ export const addNewAdmin = (data) => async (dispatch) => {
 
 export const updateProfile = (data) => async (dispatch) => {
     dispatch(userSlice.actions.updateProfileRequest());
-    await axios.put("http://localhost:4000/api/v1/user/update/profile", data,
+    await axios.put(`${API_URL}/user/update/profile`, data,
         {
             withCredentials: true,
             headers: { "Content-Type": "multipart/form-data" },
